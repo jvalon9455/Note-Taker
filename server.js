c42ff3c3073d67aa987e800aa537c3525308508f
John Alonso <jvalon9455@gmail.com>
2020-10-01 23:18:08 -0400

    post route to receive new note and add it to db.json file

--- server.js
+++ server.js
@@ -32,16 +32,19 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
     fs.readFile("db/db/json", "utf8", (err, data) =>{
         if (err) throw err;
         console.log(data);
+        const notesInput = JSON.parse(data);
+        notesInput.push(req.body);
+        console.log(notesInput);
+        fs.writeFile("./db/db.json", JSON.stringify(notesInput), (err) => {
+            if (err) throw err;
+        });
     });
 });
 
-app.post("/api/notes", (res, req) => {
-
-});
 
 app.delete("/api/notes/:id", (res, req) =>{
 
