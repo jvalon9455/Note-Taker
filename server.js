b8aed77493df664e15655318d69d874dcd4413e5
John Alonso <jvalon9455@gmail.com>
2020-10-01 22:36:19 -0400

    added requires and ports

---
+++ server.js
@@ -0,0 +1,13 @@
+// run npm install express
+// require express
+// create instance of express
+// add a PORT
+// listen on PORT
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
