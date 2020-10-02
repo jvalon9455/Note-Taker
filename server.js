ea985625a6ec0589dc64fe8875fa0de7ccbe3523
John Alonso <jvalon9455@gmail.com>
2020-10-01 23:05:05 -0400

    added routes to help return html files

--- server.js
+++ server.js
@@ -12,8 +12,37 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}`);
 });
 
 // set up routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db/json", "utf8", (err, data) =>{
+        if (err) throw err;
+        console.log(data);
+    });
+});
+
+app.post("/api/notes", (res, req) => {
+
+});
+
+app.delete("/api/notes/:id", (res, req) =>{
+
+});
