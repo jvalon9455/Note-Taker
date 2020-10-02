1fd2b5dd8c242cde003f4c305cf346525c95da2b
John Alonso <jvalon9455@gmail.com>
2020-10-02 00:32:01 -0400

    tweaking post route to work

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const db = require("./Develop/db/db.json")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -28,24 +28,22 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/api/notes", (req, res) => {
+    res.json(db);
 });
 
 app.post("/api/notes", (req, res) => {
-    fs.readFile("db/db/json", "utf8", (err, data) =>{
+    console.log(req.body);
+    fs.readFile("./Develop/db/db.json", "utf8", (err, data) => {
         if (err) throw err;
         console.log(data);
         const notesInput = JSON.parse(data);
         notesInput.push(req.body);
         console.log(notesInput);
-        fs.writeFile("./db/db.json", JSON.stringify(notesInput), (err) => {
-            if (err) throw err;
+        fs.writeFile("./Develop/db/db.json", JSON.stringify(notesInput), (err) => {
+            if(err) throw err;
         });
-    });
+    })
 });
 
 
-app.delete("/api/notes/:id", (res, req) =>{
-
-});
